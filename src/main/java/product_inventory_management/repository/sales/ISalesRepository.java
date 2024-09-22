573f0651811f3d61042cf684edb1b1209d4b8aeb
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-09-22 01:41:30 -0300

    Criação da interface do repositório de vendas

---
+++ src/main/java/product_inventory_management/repository/sales/ISalesRepository.java
@@ -0,0 +1,10 @@
+package product_inventory_management.repository.sales;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import product_inventory_management.model.sales.SalesEntity;
+
+@Repository
+public interface ISalesRepository extends JpaRepository<SalesEntity, Long> {
+
+}

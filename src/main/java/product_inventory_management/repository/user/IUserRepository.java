1e2a5e5cd145c8d9e05771e32511cfbee50f0e54
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-10-12 10:01:41 -0300

    Adiciona método de busca de usuário pelo seu username

--- src/main/java/product_inventory_management/repository/user/IUserRepository.java
+++ src/main/java/product_inventory_management/repository/user/IUserRepository.java
@@ -1,9 +1,11 @@
 package product_inventory_management.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import product_inventory_management.domain.entities.user.UserEntity;
 
 @Repository
 public interface IUserRepository extends JpaRepository<UserEntity, Integer> {
+    UserDetails findByUsername(String username);
 }

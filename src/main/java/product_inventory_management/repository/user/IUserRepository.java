c9f200ba1f9bd07eb5d1d14aa232cc8c53b892f4
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-10-12 04:46:13 -0300

    implementa repositório do usuário

---
+++ src/main/java/product_inventory_management/repository/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package product_inventory_management.repository.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import product_inventory_management.domain.entities.user.UserEntity;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserEntity, Integer> {
+}
